ea9ac7d6e3a92a9f56a1e5257b6b06621402f86d
phongbui98 <49948745+phongbui98@users.noreply.github.com>
2020-02-20 21:41:43 +0700

    dfghjkjhgfds

--- src/main/java/com/cwa/shop/dao/impl/AccountDaoImpl.java
+++ src/main/java/com/cwa/shop/dao/impl/AccountDaoImpl.java
@@ -1,4 +1,62 @@
 package com.cwa.shop.dao.impl;
 
-public class AccountDaoImpl {
+import com.cwa.shop.dao.AccountDao;
+import com.cwa.shop.model.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class AccountDaoImpl implements AccountDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public AccountDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Account> getAllAccounts() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Account> cp = cb.createQuery(Account.class);
+        Root<Account> root = cp.from(Account.class);
+        cp.select(root);
+        Query query = session.createQuery(cp);
+        return query.getResultList();
+    }
+
+    @Override
+    public void deleteAccount(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Account account = session.byId(Account.class).load(id);
+        session.delete(account);
+
+    }
+
+    @Override
+    public void saveAccount(Account account) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.saveOrUpdate(account);
+
+
+    }
+
+    @Override
+    public Account getAccount(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Account theAccount = session.get(Account.class, id);
+        return theAccount;
+
+    }
+
+
 }
--- src/main/java/com/cwa/shop/service/AccountService.java
+++ src/main/java/com/cwa/shop/service/AccountService.java
@@ -1,15 +1,12 @@
-package com.cwa.shop.dao;
+package com.cwa.shop.service;
 
 import com.cwa.shop.model.Account;
 
 import java.util.List;
 
-public interface AccountDao {
+public interface AccountService {
     List<Account> getAllAccounts();
     void deleteAccount(int id);
     void saveAccount(Account account);
     Account getAccount(int id);
-
-
-
 }
